47fa93dec6aca5c9d88bf14002bca3e58780b0f8
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-07-03 11:09:36 +0530

    Masai WebSite Webview

---
+++ app/src/main/java/com/sumit/masaiwebsite/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sumit.masaiwebsite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView browser = (WebView) findViewById(R.id.webview);
+        browser.loadUrl("https://www.masaischool.com/");
+
+
+    }
+}
